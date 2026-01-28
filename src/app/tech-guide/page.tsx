206c5e18f754684775f4a929c423f1ddc8055ce2
Prerana Kailash Pattnaik <preranapattnaik4@gmail.com>
2026-01-28 17:28:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tech-guide/page.tsx
+++ src/app/tech-guide/page.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import React from 'react';
 import { BrainCircuit, Feather, PenTool, Lightbulb, Code, Briefcase } from 'lucide-react';
 import Link from 'next/link';
 import { useState } from 'react';
